914b664058fd6791076c99253e28d9bee4e60515
Cortes-Ces <70327348+Cortes-Ces@users.noreply.github.com>
2020-08-27 19:16:00 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/* Class: CISC 3115
+ * Section:TY9
+ * EmplId:23335766
+ * Name:Cesar Cortes
+ */
+
+/**
+ * Repository GitHub Lab #1
+ */
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
