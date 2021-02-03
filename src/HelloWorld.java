637f541cba62302f0c36e72eafb348442a727e8c
Cortes-Ces <70327348+Cortes-Ces@users.noreply.github.com>
2021-02-03 11:29:23 -0500

    Create HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+/* Class: CISC 3140
+ * Section:MW2
+ * EmplId:23335766
+ * Name:Cesar Cortes
+ */
+
+/**
+ * Program prints and displays "Hello World"
+ */
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
